081a954c8c91af982f2a810d3703e9efd9f8b470
Viktor_Shcherbyna <ahmatov@ukr.net>
2026-07-20 18:13:12 +0300

    edited description in the not-found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,7 +3,7 @@ import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: '404 - Page not found | NoteHub',
-  description: 'The page is not exist',
+  description: 'Sorry, the page you are looking for does not exist',
 
   openGraph: {
     title: '404 - Page not found | NoteHub',
